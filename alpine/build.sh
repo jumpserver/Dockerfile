80b7ecb86bf03e6d32c7b04286a2f6737d648e84
liuzheng712 <liuzheng712@gmail.com>
2018-01-31 10:26:15 +0800

    feat: update

--- alpine/build.sh
+++ alpine/build.sh
@@ -11,13 +11,13 @@ yum install epel-release -y
 yum update -y 
 yum install wget unzip epel-release nginx sqlite-devel xz gcc automake zlib-devel openssl-devel redis mariadb mariadb-devel mariadb-server supervisor -y
 
-source /dev/stdin <<<  "$(curl http://${nginxip}/require.txt)" 
+source /dev/stdin <<<  "$(curl https://raw.githubusercontent.com/jumpserver/Dockerfile/mysql/alpine/require.txt)" 
 
 cd /opt/ 
-curl -o ${Python_dist} http://${nginxip}/${Python_dist}
-curl -o ${Jumpserver_dist} http://${nginxip}/${Jumpserver_dist}
-curl -o ${Coco_dist} http://${nginxip}/${Coco_dist}
-curl -o ${Luna_dist} http://${nginxip}/${Luna_dist} 
+curl -o ${Python_dist}  ${Python_URL}
+curl -o ${Jumpserver_dist}  ${Jumpserver_URL}
+curl -o ${Coco_dist} ${Coco_URL}
+curl -o ${Luna_dist} ${Luna_URL} 
 
 tar xf ${Python_dist}
 unzip ${Jumpserver_dist}
@@ -38,12 +38,12 @@ pip install -r /opt/jumpserver/requirements/requirements.txt
 pip install -r /opt/coco/requirements/requirements.txt
 
 mkdir -p /opt/mysql/ /opt/mysql/share/mysql/ /etc/nginx/
-curl -o /opt/mysql/mysql_security.sql http://${nginxip}/mysql_security.sql
-curl -o /etc/my.cnf http://${nginxip}/mysql.cnf
-curl -o /opt/mysql/share/mysql/errmsg.sys http://${nginxip}/errmsg.sys
+curl -o /opt/mysql/mysql_security.sql  ${nginxip}/mysql_security.sql
+curl -o /etc/my.cnf  ${nginxip}/mysql.cnf
+curl -o /opt/mysql/share/mysql/errmsg.sys  ${nginxip}/errmsg.sys
 
-curl -o /etc/nginx/nginx.conf http://${nginxip}/nginx.conf
-curl -o /etc/supervisord.conf http://${nginxip}/supervisord.conf
-curl -o /opt/jumpserver/config.py http://${nginxip}/jumpserver_conf.py
-curl -o /opt/start_jms.sh http://${nginxip}/start_jms.sh
+curl -o /etc/nginx/nginx.conf  ${nginxip}/nginx.conf
+curl -o /etc/supervisord.conf  ${nginxip}/supervisord.conf
+curl -o /opt/jumpserver/config.py  ${nginxip}/jumpserver_conf.py
+curl -o /opt/start_jms.sh  ${nginxip}/start_jms.sh
 
