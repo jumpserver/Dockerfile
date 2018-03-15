32568d9788ca966141e2e7ae5fdcf57e1dd068f5
liuzheng712 <liuzheng712@gmail.com>
2018-03-15 22:47:22 +0800

    feat: update some notes

--- get.sh
+++ get.sh
@@ -4,7 +4,7 @@
 # Gmail: liuzheng712
 #
 
-set -ex
+set -e
 
 echo "0. 系统的一些配置"
 setenforce 0 || true
@@ -16,43 +16,108 @@ export LC_ALL=zh_CN.UTF-8
 echo 'LANG=zh_CN.UTF-8' > /etc/sysconfig/i18n
 
 echo "1. 安装基本依赖"
+{
 yum update -y && yum install epel-release -y && yum update -y && yum install wget unzip epel-release nginx sqlite-devel xz gcc automake zlib-devel openssl-devel redis mariadb mariadb-devel mariadb-server supervisor -y
+} || {
+echo "yum出错，请更换源重新运行"
+exit 1
+}
 cd /opt/
 
 echo "2. 准备python"
+{
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz -O /opt/Python-3.6.1.tar.xz
+} || {
+echo "pyhton 依赖包下载出错，请尝试使用特殊工具进行手工下载https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz ，并且放至于/opt/Python-3.6.1.tar.xz，如您是手工下载，请注释上面wget命令再运行本脚本"
+exit 1
+}
+
+{
 tar xf Python-3.6.1.tar.xz  && cd Python-3.6.1 && ./configure && make && make install
+} || {
+echo "解压或编译python出错，请尝试使用上面的命令手工解压或编译，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
+{
 python3 -m venv py3
+} || {
+echo "建立python虚拟环境出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
 
 echo "3. 下载包并解压"
+{
 wget https://github.com/jumpserver/jumpserver/archive/1.0.0.zip -O /opt/jumpserver.zip
+} || {
+echo "下载jumpserver包出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
+{
 wget https://github.com/jumpserver/coco/archive/1.0.0.zip -O /opt/coco.zip
+} || {
+echo "下载coco包出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
+{
 wget https://github.com/jumpserver/luna/releases/download/v1.0.0/luna.tar.gz -O /opt/luna.tar.gz
+} || {
+echo "下载luna包出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
+{
 unzip coco.zip && mv coco-1.0.0 coco && unzip jumpserver.zip && mv jumpserver-1.0.0 jumpserver && tar xzf luna.tar.gz
+} || {
+echo "解压出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
 
 echo "4. 安装yum依赖"
+{
 yum -y install $(cat /opt/jumpserver/requirements/rpm_requirements.txt) && yum -y install $(cat /opt/coco/requirements/rpm_requirements.txt)
+} || {
+echo "安装jumpserver的依赖出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
 
 echo "5. 安装pip依赖"
+{
 source /opt/py3/bin/activate && pip install --upgrade pip && pip install -r /opt/jumpserver/requirements/requirements.txt &&  pip install -r /opt/coco/requirements/requirements.txt
-
+} || {
+echo "安装jumpserver的依赖出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
 echo "6. 创建数据库"
 mkdir -p /opt/mysql/share/mysql/
+{
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/mysql_security.sql?raw=true -O /opt/mysql/mysql_security.sql
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/mysql.cnf?raw=true -O /etc/my.cnf
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/errmsg.sys?raw=true -O /opt/mysql/share/mysql/errmsg.sys
+} || {
+echo "下载数据库依赖文件出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
 
 echo "7. 准备文件"
+{
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/nginx.conf?raw=true -O /etc/nginx/nginx.conf
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/supervisord.conf?raw=true -O /etc/supervisord.conf
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/jumpserver_conf.py?raw=true -O /opt/jumpserver/config.py
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/coco_conf.py?raw=true -O /opt/coco/conf.py
 wget https://github.com/jumpserver/Dockerfile/blob/mysql/alpine/start_jms.sh?raw=true -O /opt/start_jms.sh
-
+} || {
+echo "下载配置文件出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
 echo "8. 安装docker"
 yum check-update
+{
 curl -fsSL https://get.docker.com/ | sh
-systemctl start docker 
+} || {
+echo "安装docker 出错，请尝试手工执行，如手工操作成功，请注释上述代码再运行本脚本"
+exit 1
+}
+
+systemctl start docker
 systemctl enable docker
 
 echo "9. 安装guacamole"
@@ -66,7 +131,7 @@ docker run --name jms_guacamole -d \
   registry.jumpserver.org/public/guacamole:1.0.0
 
 echo "10. 配置nginx"
-cat <<EOF> /etc/nginx/conf.d/jumpserver.conf 
+cat << EOF > /etc/nginx/conf.d/jumpserver.conf
 server {
     listen 80;
 
@@ -113,10 +178,18 @@ server {
 
 EOF
 
-mkdir -p /opt/nginx && chmod 777 /opt/nginx
+mkdir -p /opt/nginx/log && chmod -R 777 /opt/nginx
+{
 systemctl restart nginx
 systemctl enable nginx
-
+} || {
+service restart nginx
+} || {
+nginx -s reload
+} || {
+echo "请检查nginx的启动命令"
+exit 1
+}
 
 
 echo " 安装完成，请运行/opt/start_jms.sh启动jumpserver"
