46bdbbe0f6fb427997f82c7a022d9af3b0df4e87
wojiushixiaobai <296015668@qq.com>
2019-10-24 17:39:56 +0800

    [Update]更新

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export LANG=zh_CN.UTF-8
 
 if [ $DB_HOST == 127.0.0.1 ]; then
     if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
-        mysql_install_db --user=mysql --datadir=/var/lib/mysql
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql --force
         mysqld_safe &
         sleep 5s
         mysql -uroot -e "create database jumpserver default charset 'utf8'; grant all on jumpserver.* to 'jumpserver'@'127.0.0.1' identified by '$DB_PASSWORD'; flush privileges;"
