e46361b7528f804dba08e06c2c06272f42384947
wojiushixiaobai <296015668@qq.com>
2019-12-27 22:41:54 +0800

    [Update]更新

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,6 +35,7 @@ if [ ! -f "/opt/jumpserver/config.yml" ]; then
     sed -i "s/REDIS_HOST: 127.0.0.1/REDIS_HOST: $REDIS_HOST/g" /opt/jumpserver/config.yml
     sed -i "s/REDIS_PORT: 6379/REDIS_PORT: $REDIS_PORT/g" /opt/jumpserver/config.yml
     sed -i "s/# REDIS_PASSWORD: /REDIS_PASSWORD: $REDIS_PASSWORD/g" /opt/jumpserver/config.yml
+    sed -i "s/# WINDOWS_SKIP_ALL_MANUAL_PASSWORD: False/WINDOWS_SKIP_ALL_MANUAL_PASSWORD: True/g" /opt/jumpserver/config.yml
 fi
 
 if [ ! -f "/opt/koko/config.yml" ]; then
