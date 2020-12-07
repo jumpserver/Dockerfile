8446137b6044e5a64bcf68e2b6b76c730ac473ce
wojiushixiaobai <296015668@qq.com>
2020-12-07 15:23:01 +0800

    fix: 修复 dockerfile

--- guacamole/entrypoint.sh
+++ guacamole/entrypoint.sh
@@ -14,10 +14,10 @@ if [ ! $JUMPSERVER_ENABLE_DRIVE ]; then
     export JUMPSERVER_ENABLE_DRIVE=true
 fi
 if [ ! $JUMPSERVER_RECORD_PATH ]; then
-    export JUMPSERVER_RECORD_PATH=/config/guacamole/data/record/
+    export JUMPSERVER_RECORD_PATH=/config/guacamole/data/record
 fi
 if [ ! $JUMPSERVER_DRIVE_PATH ]; then
-    export JUMPSERVER_DRIVE_PATH=/config/guacamole/data/drive/
+    export JUMPSERVER_DRIVE_PATH=/config/guacamole/data/drive
 fi
 if [ ! $JUMPSERVER_CLEAR_DRIVE_SESSION ]; then
     export JUMPSERVER_CLEAR_DRIVE_SESSION=true
