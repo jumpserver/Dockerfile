19059e1898f008b8e15b8a78330a217d4bd978b8
吴小白 <296015668@qq.com>
2022-01-12 14:32:23 +0800

    fix: 修复 websocket 错误

--- allinone/entrypoint.sh
+++ allinone/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-
+echo
 while ! nc -z $DB_HOST $DB_PORT;
 do
     echo "wait for jms_mysql ${DB_HOST} ready"
     sleep 2s
 done
 
-if [ $REDIS_HOST == 127.0.0.1 ]; then
-    sed -i "s@# requirepass .*@requirepass $REDIS_PASSWORD@g" /etc/redis.conf
+if [ $REDIS_HOST == "127.0.0.1" ]; then
+    sed -i "s@# requirepass .*@requirepass $REDIS_PASSWORD@g" /etc/redis/redis.conf
     /etc/init.d/redis-server start
 fi
 
@@ -22,15 +22,31 @@ if [ ! -f "/opt/jumpserver/config.yml" ]; then
     echo > /opt/jumpserver/config.yml
 fi
 
+if [ ! -f "/opt/jumpserver/logs/jumpserver.log" ]; then
+    echo > /opt/jumpserver/logs/jumpserver.log
+    chmod 644 /opt/jumpserver/logs/jumpserver.log
+fi
+
+if [ ! -d "/opt/jumpserver/data/media/replay" ]; then
+   mkdir -p /opt/jumpserver/data/media/replay
+   chmod 755 -R /opt/jumpserver/data/media/replay
+fi
+
 if [ ! -d "/opt/jumpserver/data/static" ]; then
     mkdir -p /opt/jumpserver/data/static
     chmod 755 -R /opt/jumpserver/data/static
 fi
 
-. /opt/py3/bin/activate
-cd /opt/jumpserver && ./jms start -d
+sed -i "s@Version:*@Version: ${Version}@g" /opt/readme.txt
+rm -f /opt/jumpserver/tmp/*.pid
+
 /etc/init.d/supervisor start
 /etc/init.d/nginx start
 
-echo "Jumpserver ALL $Version"
-tail -f /opt/readme.txt
+echo
+echo "Time: $(date "+%Y-%m-%d %H:%M:%S")"
+cat /opt/readme.txt
+echo
+echo "LOG_LEVEL: ${LOG_LEVEL}"
+echo "JumpServer Logs:"
+tail -f /opt/jumpserver/logs/jumpserver.log
