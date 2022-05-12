9403e6c6e8541a4cbdccadf6f6a1f07c2558e497
吴小白 <296015668@qq.com>
2022-05-12 12:39:39 +0800

    fix: 修正 magnus 启动错误

--- magnus/entrypoint.sh
+++ magnus/entrypoint.sh
@@ -7,9 +7,13 @@ do
     sleep 2
 done
 
+export WORK_DIR=/opt/magnus
+export COMPONENT_NAME=magnus
+export EXECUTE_PROGRAM=/opt/magnus/magnus
+
 if [ ! "$LOG_LEVEL" ]; then
     export LOG_LEVEL=ERROR
 fi
 
 cd /opt/magnus
-./magnus
+wisp
