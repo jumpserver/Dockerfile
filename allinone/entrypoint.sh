052fd3f3159f1f05f20fa02e6f51087e297de4a4
吴小白 <296015668@qq.com>
2023-09-23 01:46:17 +0800

    perf: 修正 chen 组件下载地址

--- allinone/entrypoint.sh
+++ allinone/entrypoint.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 #
 action="${1}"
+
+if [[ "$action" == "bash" || "$action" == "sh" ]]; then
+    bash
+    exit 0
+fi
+
 echo
-while ! nc -z $DB_HOST $DB_PORT;
+
+if [ ! "${DB_HOST}" ] || [ ! "${DB_PORT}" ] || [ ! "${DB_USER}" ] || [ ! "${DB_PASSWORD}" ] || [ ! "${DB_NAME}" ]; then
+    echo -e "\033[31m Please set database environment \033[0m"
+    exit 1
+fi
+
+while ! nc -z "${DB_HOST}" "${DB_PORT}";
 do
     echo "wait for jms_mysql ${DB_HOST} ready"
     sleep 2s
 done
 
-while ! nc -z $REDIS_HOST $REDIS_PORT;
+while ! nc -z "${REDIS_HOST}" "${REDIS_PORT}";
 do
     echo "wait for jms_redis ${REDIS_HOST} ready"
     sleep 2s
@@ -34,19 +46,21 @@ fi
 
 rm -f /opt/jumpserver/tmp/*.pid
 
+if [ ! "${LOG_LEVEL}" ]; then
+    export LOG_LEVEL=ERROR
+fi
+sed -i "s@root: INFO@root: ${LOG_LEVEL}@g" /opt/chen/config/application.yml
+sed -i "s@address: static://127.0.0.1:9090@address: static://127.0.0.1:9092@g" /opt/chen/config/application.yml
+
 if [ -f "/etc/init.d/cron" ]; then
   /etc/init.d/cron start
 fi
 
-if [ "$action" == "upgrade" ] || [ "$action" == "init_db" ]; then
-    cd /opt/jumpserver
-    . /opt/py3/bin/activate
-    ./jms upgrade_db || {
-        echo -e "\033[31m 数据库处理失败, 请根据错误排查问题. \033[0m"
-        exit 1
-   }
-   exit 0
-fi
+cd /opt/jumpserver || exit 1
+./jms upgrade_db || {
+    echo -e "\033[31m Failed to change the table structure. \033[0m"
+    exit 1
+}
 
 echo
 echo "Time: $(date "+%Y-%m-%d %H:%M:%S")"
@@ -54,6 +68,7 @@ if [ -f "/opt/readme.txt" ]; then
   cat /opt/readme.txt
   rm -f /opt/readme.txt
 fi
+
 echo
 echo "LOG_LEVEL: ${LOG_LEVEL}"
 echo "JumpServer Logs:"
--- chen/entrypoint.sh
+++ chen/entrypoint.sh
@@ -10,11 +10,13 @@ done
 export WORK_DIR=/opt/chen
 export COMPONENT_NAME=chen
 export WISP_TRACE_PROCESS=1
-export EXECUTE_PROGRAM="java -Dfile.encoding=utf-8 --add-opens java.base/jdk.internal.loader=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAME -XX:+ExitOnOutOfMemoryError  -jar /opt/chen/chen.jar --mock.enable=false"
+export EXECUTE_PROGRAM="java -Dfile.encoding=utf-8 --add-opens java.base/jdk.internal.loader=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAME -XX:+ExitOnOutOfMemoryError -jar /opt/chen/chen.jar --mock.enable=false"
 
 if [ ! "$LOG_LEVEL" ]; then
-    export LOG_LEVEL=ERROR
+    LOG_LEVEL=ERROR
 fi
+
+sed -i "s@root: INFO@root: ${LOG_LEVEL}@g" /opt/chen/config/application.yml
 
 cd /opt/chen
 wisp
